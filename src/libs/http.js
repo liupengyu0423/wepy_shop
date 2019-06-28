839b6160fdaa4be7d73cb3e8b25cdfc3c163155d
liupengyu <liupengyu@thunder.com.cn>
2019-06-28 13:16:36 +0800

    wepy首次构建

---
+++ src/libs/http.js
@@ -0,0 +1,15 @@
+
+/*
+* http接口中的参数params说明
+* param = {
+*   url:'',
+*   data:{},
+*   header:{},
+*   method:'',
+*   dataType:'json'
+* }
+*/
+import wepy from 'wepy'
+export default class HTTP {
+
+}
